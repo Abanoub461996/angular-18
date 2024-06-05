eccd9a1e45f185bebf3b1582febdbe12eaa97e6b
Abanoub461996 <pophtc.aa@gmail.com>
2024-06-05 23:40:07 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShareDataComponent } from './modules/share-data/share-data.component';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ShareDataComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-18';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
